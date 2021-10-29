2505d0ad8949f40c241ca681292b0309a58e8c49
MarcelBienko3b <90909175+MarcelBienko3b@users.noreply.github.com>
2021-10-29 15:04:53 +0200

    Repaired

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-const num1 = document.querySelector('#num1').value;
-const num2 = document.querySelector('#num2').value;
+const num1 = document.querySelector('#num1');
+const num2 = document.querySelector('#num2');
 
 const btn = document.querySelector('button');
 const p = docuemnt.querySelector('p');
 
 btn.addEventListener('click', function(){
-    const suma = parseInt('num1') + parseInt('num2');
+    const suma = parseInt('num1.value') + parseInt('num2.value');
     p.innerHTML = `Suma wynosi: <b> ${suma} </b>`;
 })
