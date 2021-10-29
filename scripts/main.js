81a89b04bdc6db882db3803942ceec736e7e0e9c
MarcelBienko3b <90909175+MarcelBienko3b@users.noreply.github.com>
2021-10-29 15:12:13 +0200

    123

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const num1 = document.querySelector('#num1');
 const num2 = document.querySelector('#num2');
 
 const btn = document.querySelector('button');
-const p = docuemnt.querySelector('p');
+const p = document.querySelector('p');
 
 btn.addEventListener('click', function(){
     const suma = parseInt(num1.value) + parseInt(num2.value);
