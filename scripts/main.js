8f309c0bc9281a1372ac1102678f4dd27d6f3dae
MarcelBienko3b <90909175+MarcelBienko3b@users.noreply.github.com>
2021-10-29 15:02:55 +0200

    Added complete JS script

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
-var num1 = document.getElementById("num1");
-var num2 = document.getElementById("num2");
+const num1 = document.querySelector('#num1').value;
+const num2 = document.querySelector('#num2').value;
 
-console.log(num1, num2);
+const btn = document.querySelector('button');
+const p = docuemnt.querySelector('p')
+
+btn.addEventListener('click', function(){
+    const suma = parseInt('num1') + parseInt('num2');
+    p.innerHTML = `Suma wynosi: <b> ${suma} </b>`;
+})
